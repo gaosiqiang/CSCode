262ee334c639b8322945f24aa605666e66c1cd66
gaosiqiang <gaosiqiang7@163.com>
2022-06-21 22:28:11 +0800

    revision)优化了文件

--- data_type_array_1.cpp
+++ data_type_array_1.cpp
@@ -1,44 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main ()
+//数组
+int main(int argc, char const *argv[])
 {
-    cout << "start" << endl;
+	
+	//声明数组a
+	char a[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+	//引用
+	cout << a[1];//b
+	cout << a[7];//h
+	cout << a[0];//a
+	//修改
+	a[2] = 'x';
+	a[1] = 'y';
+	a[5] = 'z';
+	cout << endl;
+	cout << a[2];//b
+	cout << a[1];//h
+	cout << a[5];//a
 
-    //定义指定索引范围的数组
-    int a[2] = {1, 2};
-    //定义动态索引范围的数组
-    int b[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    //访问数组元素，标准输出
-    cout << a[1] << endl; //2
-    cout << b[2] << endl; //3
+	cout << endl;
+	//打印出序号为奇数的元素
+	for (int i = 1; i <= 5; ++i)
+	{
+		if (i % 2 != 0) {
+			cout << a[i] << endl;
+		}
+	}
 
-    //定义二维数组
-    int c[2][3] = {
-        {1, 2, 3},
-        {4, 5, 6}
-    };
-    //二维数组的访问
-    cout << c[0][1] << endl; //2
-    c[0][1] = 9;
-    cout << c[0][1] << endl; //9
-
-    //定义三维数组
-    int d[2][3][3] = {
-        {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        },
-        {
-            {10, 11, 12},
-            {13, 14, 15},
-            {16, 17, 18}
-        }
-    };
-    //访问三维数组
-    cout << d[0][1][2] << endl; //6
-
-    cout << "end" << endl;
-    return 0;
+	return 0;
 }
--- operator_1.cpp
+++ operator_1.cpp
@@ -22,7 +22,7 @@ int main()
     cout << "a|b二进制：" << bitset<numeric_limits<unsigned int>::digits>(c) << endl; //二进制输出变量c
     c = a ^ b;
     cout << "a^b二进制：" << bitset<numeric_limits<unsigned int>::digits>(c) << endl; //二进制输出变量c
-    c = ~a;
+    c = ~a; //位运算
     cout << "~a 二进制：" << bitset<numeric_limits<unsigned int>::digits>(c) << endl; //二进制输出变量c
     c = a << 2;
     cout << "a<<2二进制：" << bitset<numeric_limits<unsigned int>::digits>(c) << endl; //二进制输出变量c
