f2aa5fc519f25acdaf385ea31b5412e640eb2775
gaosiqiang <gaosiqiang7@163.com>
2022-06-24 21:02:58 +0800

    revision)代码优化

--- pointer_1.cpp
+++ pointer_1.cpp
@@ -5,19 +5,21 @@ using namespace std;
 //指针
 int main()
 {
+    cout << "start" << endl;
     int a = 100;
-    cout << &a << endl; //输出变量a的指针
-    cout << typeid(a).name() << endl; //输出i表示是int类型
-    cout << typeid(&a).name() << endl; //输出Pi表示类型是指针，指向整数int资源的指针
+    cout << "a的指针是：" << &a << endl; //输出变量a的指针
+    cout << "a的值是：" << typeid(a).name() << endl; //输出i表示是int类型
+    cout << "a的指针的数据类型是：" << typeid(&a).name() << endl; //输出Pi表示类型是指针，指向整数int资源的指针
 
     char b;
     b = 'a';
-    cout << &b << endl; //输出变量b的指针
-    cout << typeid(b).name() << endl; //输出c表示是char类型
-    cout << typeid(&b).name() << endl; // 输出Pc表示类型是指针，指向字符char资源的指针
+    cout << "b的指针是：" << &b << endl; //输出变量b的指针
+    cout << "b的值是：" << typeid(b).name() << endl; //输出c表示是char类型
+    cout << "b的指针的数据类型是：" << typeid(&b).name() << endl; // 输出Pc表示类型是指针，指向字符char资源的指针
 
-    cout << sizeof(&a) << endl; //输出该指针在内存中的存储多个个字节空间
-    cout << sizeof(&b) << endl; //输出该指针在内存中的存储多个个字节空间
+    cout << "a的指针大小是：" << sizeof(&a) << "个字节" << endl; //输出该指针在内存中的存储多个个字节空间
+    cout << "a的指针大小是：" << sizeof(&b) << "个字节" << endl; //输出该指针在内存中的存储多个个字节空间
 
+    cout << "end" << endl; //最后输出需要回车就加上endl操作符了
     return 0;
 }
