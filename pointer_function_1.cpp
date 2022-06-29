b07f0e4f796bc853667931e6af557ebc5af8e274
gaosiqiang <gaosiqiang7@163.com>
2022-06-29 17:40:32 +0800

    revision code

--- pointer_function_1.cpp
+++ pointer_function_1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void Rank1(int *p1, int *p2)
+void Rank(int *p1, int *p2)
 {
     int temp;
     if (*p1 > *p2) {
@@ -18,7 +18,7 @@ int main()
     int a = 2, b = 1;
     int *p1 = NULL, *p2 = NULL;
     p1 = &a, p2 = &b;
-    Rank1(p1, p2);
+    Rank(p1, p2);
     cout << a << endl; // 1
     cout << b << endl; // 2
     return 0;
