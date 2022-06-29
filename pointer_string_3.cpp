a6ad72e87c221a4abe49e7ba17f0e4aacf3c2bc3
gaosiqiang <gaosiqiang7@163.com>
2022-06-29 14:48:22 +0800

    revision code

--- pointer_string_3.cpp
+++ pointer_string_3.cpp
@@ -4,14 +4,18 @@
 
 using namespace std;
 
-//指针与字符串-1
+//指针与字符串-3
 int main()
 {
-  char buffer[100] = "ABC";
-  const char *pc = NULL;
-  pc = "DEF";
-  cout << pc << endl;
-  cout << buffer << endl;
+    char buffer[100] = "ABC";
+    const char *pc = NULL;
+    pc = "DEF"; //字符类型指针赋值字符串，可以使用const来修饰也可以不用，具体看编译器；因为字符串字面值是常量，c++中禁止将字符串常亮赋给`char *指针`，如果是`char *p = "hello world";`，有些编译器会报错；
+    cout << pc << endl; //DEF
+    pc++;
+    cout << pc << endl; //EF
+    cout << *pc << endl; //E
+    pc = buffer;
+    cout << pc << endl; //ABC
 
-  return 0;
+    return 0;
 }
