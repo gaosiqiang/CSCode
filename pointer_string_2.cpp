279ddb104428a933e08bfedfba5a3c89e73b9fbc
gaosiqiang <gaosiqiang7@163.com>
2022-06-29 14:58:05 +0800

    revision code

--- pointer_string_2.cpp
+++ pointer_string_2.cpp
@@ -26,9 +26,9 @@ int main()
     cout << c << endl;
     cout << pc << endl;
     cout << d << endl; //会输出'abcd'；cout操作符会对字符数组元素自动进行拼接处理；cout只有字符类型的才这样处理；
-    cout << pd << endl; ////会输出'abcd'；与上面的同理；
-    cout <<static_cast<void*>(c) << endl; //0x7fff27a18c20
-    cout <<static_cast<void*>(pc) << endl; //0x7fff27a18c20
+    cout << pd << endl; //会输出'abcd'；与上面的同理；
+    cout <<static_cast<void*>(c) << endl; //输出地址：0x7fff27a18c20
+    cout <<static_cast<void*>(pc) << endl; //输出地址：0x7fff27a18c20
 
     return 0;
 }
