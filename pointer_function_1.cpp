d1196960f5ea91c22791f3aba4707c57cf8257b4
gaosiqiang <gaosiqiang7@163.com>
2022-06-29 16:49:06 +0800

    +)add file

---
+++ pointer_function_1.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+
+using namespace std;
+
+void Rank1(int *p1, int *p2)
+{
+    int temp;
+    if (*p1 > *p2) {
+        temp = *p1;
+        *p1 = *p2;
+        *p2 = temp;
+    }
+    return;
+}
+
+int main()
+{
+    int a = 2, b = 1;
+    int *p1 = NULL, *p2 = NULL;
+    p1 = &a, p2 = &b;
+    Rank1(p1, p2);
+    cout << a << endl; // 1
+    cout << b << endl; // 2
+    return 0;
+}
